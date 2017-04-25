24f01c26e476eed9cbda2f2a0ed5be5be462d0dc
cristian arroyave <cdarroyaveg@uniremingtonmanizales.edu.co>
2017-04-24 20:43:21 -0500

    pasar de una actividad a otra

--- app/src/main/java/com/example/c_musicproducciones/proyecto/MainActivity.java
+++ app/src/main/java/com/example/c_musicproducciones/proyecto/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.c_musicproducciones.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView texto1;
     Button boton1;
-    Button boton2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         texto1 = (TextView) findViewById(R.id.view1);
-        boton1 = (Button) findViewById(R.id.button2);
-        boton2 = (Button) findViewById(R.id.button3);
+        boton1 = (Button) findViewById(R.id.siguiente);
 
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case  R.id.siguiente:
+                Intent intent1 =new Intent(this,Main2Activity.class);
+                startActivity(intent1);
+        }
     }
 }
