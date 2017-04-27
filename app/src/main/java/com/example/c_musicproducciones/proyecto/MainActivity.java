31fca7313463f79d13787438a3bdb5ac3fb05cfd
cristian arroyave <cdarroyaveg@uniremingtonmanizales.edu.co>
2017-04-26 22:14:49 -0500

    creacion de los radio buttons

--- app/src/main/java/com/example/c_musicproducciones/proyecto/MainActivity.java
+++ app/src/main/java/com/example/c_musicproducciones/proyecto/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.c_musicproducciones.proyecto;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView texto1;
     Button boton1;
+
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boton1 = (Button) findViewById(R.id.siguiente);
         boton1.setOnClickListener(this);
 
-
+        radioGroup = (RadioGroup)findViewById(R.id.radiog);
+        this.radios();
     }
 
     @Override
@@ -32,4 +38,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent1);
         }
     }
+
+
+   public void radios ()
+   {
+       radioGroup.setOnClickListener(new RadioGroup.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+
+               switch (checkedId)
+               {
+                   case R.id.rbtn1:
+
+                   Toast.makeText(getApplicationContext(),"Preproduccion",Toast.LENGTH_SHORT).show();
+                       return;
+                   case R.id.rbtn2:
+
+                       Toast.makeText(getApplicationContext(),"Produccion",Toast.LENGTH_SHORT).show();
+                       return;
+
+                   case R.id.rbtn3:
+
+
+                       Toast.makeText(getApplicationContext(),"Postproduccion",Toast.LENGTH_SHORT).show();
+                       return;
+
+                   default:
+                       break;
+               }
+
+
+
+           }
+       });
+
+   }
+   //
 }
